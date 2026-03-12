160f3c55f60a4a3e8b2c2454f07d8651664e0c6c
DuongTuanKhang <khangnha0811@gmail.com>
2026-03-12 14:26:35 +0700

    A1 create base layout for floral page

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 
-app.use(express.static("public"));
+app.set("view engine", "ejs")
+
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
+    res.render("index")
+})
 
 app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
-});
+    console.log("Server running on port 3000")
+})
